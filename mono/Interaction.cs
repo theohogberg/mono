f5e9fb551d7e1c96db0781fca1192ac54c6005ab
theo <theoswe@gmail.com>
2020-01-04 14:30:13 +0100

    .gitignore update

---
+++ mono/Interaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mono
+{
+    public class Interaction
+    {
+        public Interaction(List<Character> lc)
+        {
+
+        }
+    }
+}
